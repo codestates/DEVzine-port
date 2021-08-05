de3b449df58bcefc4b21ae59a50c44b8e0a4b46f
Haeun Kang <tejavascript@gmail.com>
2021-08-05 09:00:41 +0900

    Add DB database option

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ mongoose
     useUnifiedTopology: true, // shard 와 replica set 에 접근
     useCreateIndex: true, // deprecated 된 ensureIndex 대신 createIndex 사용 
     useFindAndModify: false, // findOneAndRemove() 과 findOneAndUpdate() 를 분리해서 사용 
-    // dbName: "userInfo" // connection string 에 있는 db 대신 다른 디폴트 db 지정 
+    dbName: process.env.MONGO_DATABASE // connection string 에 있는 db 대신 다른 디폴트 db 지정 
   })
   .then(() => console.log(`mongoDB connected`))
   .catch((err) => console.error(err));
