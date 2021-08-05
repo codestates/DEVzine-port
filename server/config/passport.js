e2572d81bce3a0a327d8e9f71e99612ebc6111ba
hyoogu <idhyo0o@gmail.com>
2021-08-05 17:56:07 +0900

    Fix DB name

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/Users');
+const { User } = require('../models/Users');
 
 module.exports = () => {
 
@@ -14,25 +14,27 @@ module.exports = () => {
 	// 세션에 저장한 아이디를 통해서 사용자 정보 객체 불러옴
 	// req.user 객체가 생성되어 유저에 대한 정보 조회 가능
 	passport.deserializeUser((id, done) => {
-		Test.findById(id, (err, user) => {
+		User.findById(id, (err, user) => {
 			done(err, user);
 		});
 	});
 	
 	passport.use(new LocalStrategy({
-		usernameField: 'id', // id set
-		passwordField: 'password', // password set
+		usernameField: 'user_email', // id set
+		passwordField: 'user_password', // password set
 		session: true, // session set
 		passReqToCallback: false // callback req off
-	}, (id, password, done) => {
-		Test.findOne({ id }, (err, user) => {
+	}, (username, password, done) => {
+		User.findOne({ user_email: username }, (err, user) => {
+			// console.log(user);
 			if (err) { // 에러가 발생하면 에러를 반환
 				return done(err);
 			}
 			if (!user) { // 유저가 없으면 에러를 반환
 				return done(null, false, { message: 'Incorrect username.' });
 			}
-			if (!user.validPassword(password)) { // 유저의 패스워드가 일치하지 않으면 에러를 반환
+			// ! 패스워드 비교 함수 작성 필요 지금처럼 단순 비교x
+			if (user.password !== password) { // 유저의 패스워드가 일치하지 않으면 에러를 반환
 				return done(null, false, { message: 'Incorrect password.' });
 			}
 			return done(null, user); // 정상적으로 세션에 저장된 유저의 정보를 반환
