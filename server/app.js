570d971ce377f92a636b2ff3b722190de55d95fc
Haeun Kang <tejavascript@gmail.com>
2021-08-06 15:56:15 +0900

    Test DB - 3

--- server/app.js
+++ server/app.js
@@ -47,16 +47,16 @@ app.use(
   })
 );
 
-// mongoose
-//   .connect(process.env.MONGO_STRING, {
-//     useNewUrlParser: true, // 버전 5 이상부터 적용되는 새로운 url parser 사용
-//     useUnifiedTopology: true, // shard 와 replica set 에 접근
-//     useCreateIndex: true, // deprecated 된 ensureIndex 대신 createIndex 사용 
-//     useFindAndModify: false, // findOneAndRemove() 과 findOneAndUpdate() 를 분리해서 사용 
-//     dbName: process.env.MONGO_DATABASE, // connection string 에 있는 db 대신 다른 디폴트 db 지정 
-//   })
-//   .then(() => console.log(`mongoDB connected`))
-//   .catch((err) => console.error(err));
+mongoose
+  .connect(process.env.MONGO_STRING, {
+    useNewUrlParser: true, // 버전 5 이상부터 적용되는 새로운 url parser 사용
+    useUnifiedTopology: true, // shard 와 replica set 에 접근
+    useCreateIndex: true, // deprecated 된 ensureIndex 대신 createIndex 사용 
+    useFindAndModify: false, // findOneAndRemove() 과 findOneAndUpdate() 를 분리해서 사용 
+    dbName: process.env.MONGO_DATABASE, // connection string 에 있는 db 대신 다른 디폴트 db 지정 
+  })
+  .then(() => console.log(`mongoDB connected`))
+  .catch((err) => console.error(err));
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 80;
 let server;
