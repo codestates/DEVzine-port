dd14fd8fef27bdf99ad4ee69f13bd8ba4f28ae4f
Haeun Kang <tejavascript@gmail.com>
2021-08-06 19:44:23 +0900

    Test DB - 7

--- server/controller/server_test/test.js
+++ server/controller/server_test/test.js
@@ -43,7 +43,7 @@ module.exports = {
 			// await newUser.save();
 			// await newArticle.save();
 			// await newTest.save();
-			return res.status(200).send('ok2')
+			return res.status(200).send('ok3')
 			// return res.status(200).json({"message": "db successfully connected!", "data" : {
 			// 	newTest, newArticle, newUser, newSubscriber, newContribution
 			// }});
