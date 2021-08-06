01bb9901e604de504fe6cd8f7826add34a87e96f
Haeun Kang <tejavascript@gmail.com>
2021-08-06 20:26:55 +0900

    Test DB - 14

--- server/controller/server_test/test.js
+++ server/controller/server_test/test.js
@@ -43,13 +43,21 @@ module.exports = {
 			// await newUser.save();
 			// await newArticle.save();
 			// await newTest.save();
-			await Article.create({
-					article_title: randomText,
-					article_content: 'test',
-					article_date: Date.now(),
-					article_keyword: 'coding',
-					article_url: randomText
-				})
+			const dummy = [{
+				article_title: randomText,
+				article_content: 'test',
+				article_date: Date.now(),
+				article_keyword: 'coding',
+				article_url: randomText
+			}, {
+				article_title: 'please',
+				article_content: 'test',
+				article_date: Date.now(),
+				article_keyword: 'coding',
+				article_url: 'please'
+			}]
+			await Article.create(dummy)
+
 			return res.status(200).send('please')
 			// return res.status(200).json({"message": "db successfully connected!", "data" : {
 			// 	newTest, newArticle, newUser, newSubscriber, newContribution
