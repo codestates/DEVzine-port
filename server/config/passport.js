84b3cb980ead7438d3f91ddce19b9574203fa71a
hyoogu <idhyo0o@gmail.com>
2021-08-07 16:13:56 +0900

    Add test dummy data

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,12 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const { User } = require('../models/Users');
 
+const dummyUser = {
+	user_name: 'test',
+	user_password: 'qwer1234',
+	user_email: 'test@test.com'
+}
+
 module.exports = () => {
 
 	// 로그인을 성공했을 때 딱 한 번 callback 함수가 실행
@@ -25,21 +31,36 @@ module.exports = () => {
 		session: true, // session set
 		passReqToCallback: false // callback req off
 	}, (username, password, done) => {
-		User.findOne({ user_email: username }, (err, user) => {
-			// console.log(user);
-			if (err) { // 에러가 발생하면 에러를 반환
-				return done(err);
-			}
-			if (!user) { // 유저가 없으면 에러를 반환
-				return done(null, false, { message: 'Incorrect username.' });
-			}
-			// ! 패스워드 비교 함수 작성 필요 지금처럼 단순 비교x
-			if (user.password !== password) { // 유저의 패스워드가 일치하지 않으면 에러를 반환
-				return done(null, false, { message: 'Incorrect password.' });
-			}
-			return done(null, user); // 정상적으로 세션에 저장된 유저의 정보를 반환
-			// 인증이성공하면 passport에 사용자의 정보를 전달
-		});
+		let user = {
+			_id: 1,
+			user_name: username,
+			user_password: password
+		}
+		if (dummyUser.user_name !== username) {
+			return done(null, false, { message: 'Incorrect username.' });
+		}
+		if (dummyUser.user_password !== password) {
+			return done(null, false, { message: 'Incorrect password.' });
+		}
+		return done(null, user);
+
+
+		// db 조회 ver
+		// User.findOne({ user_email: username }, (err, user) => {
+		// 	if (err) { // 에러가 발생하면 에러를 반환
+		// 		return done(err);
+		// 	}
+		// 	if (!user) { // 유저가 없으면 에러를 반환
+		// 		return done(null, false, { message: 'Incorrect username.' });
+		// 	}
+		// 	// ! 패스워드 비교 함수 작성 필요 지금처럼 단순 비교x
+		// 	// console.log(user.password);
+		// 	if (user.user_password !== password) { // 유저의 패스워드가 일치하지 않으면 에러를 반환
+		// 		return done(null, false, { message: 'Incorrect password.' });
+		// 	}
+		// 	return done(null, user); // 정상적으로 세션에 저장된 유저의 정보를 반환
+		// 	// 인증이성공하면 passport에 사용자의 정보를 전달
+		// });
 	}));
 
 };
