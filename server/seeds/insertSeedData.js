2b0aea6039da45c8fff54731634363bf48932e8f
jong-ah <jonga6431@gmail.com>
2021-08-09 12:06:36 +0900

    Fix typo Modal modal

--- server/seeds/insertSeedData.js
+++ server/seeds/insertSeedData.js
@@ -1,25 +1,22 @@
-const { Article } = require('../models/Articles');
+const { Article } = require('../Models/Articles');
 const { seedArticles } = require('./seed-articles');
-const { Contribution } = require('../models/Contributions');
+const { Contribution } = require('../Models/Contributions');
 const { seedContributions } = require('./seed-contributions');
-const { Subscriber } = require('../models/Subscribers');
+const { Subscriber } = require('../Models/Subscribers');
 const { seedSubscribers } = require('./seed-subscribers');
-const { User } = require('../models/Users');
+const { User } = require('../Models/Users');
 const { seedUsers } = require('./seed-users');
 
 module.exports = {
-
-    insertSeedData :  async (req, res) => {
-        try{
-            await Article.create(seedArticles);
-            await Contribution.create(seedContributions);
-            await Subscriber.create(seedSubscribers);
-            await User.create(seedUsers);
-            res.json({"message" : "Seed success"});
-        } catch(err){
-            res.json(err);
-        }
-
+  insertSeedData: async (req, res) => {
+    try {
+      await Article.create(seedArticles);
+      await Contribution.create(seedContributions);
+      await Subscriber.create(seedSubscribers);
+      await User.create(seedUsers);
+      res.json({ message: 'Seed success' });
+    } catch (err) {
+      res.json(err);
     }
-
-}
+  },
+};
