042bf9690dc5998536fceddc48ae8dfffa058059
jong-ah <jonga6431@gmail.com>
2021-08-09 12:03:44 +0900

    Add react-router-dom

---
+++ client/src/pages/ArticleView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ArticleViewWrapper from '../components/ArticleView/ArticleViewWrapper';
+
+function ArticleView() {
+  return (
+    <>
+      ArticleView
+      <ArticleViewWrapper />
+    </>
+  );
+}
+
+export default ArticleView;
