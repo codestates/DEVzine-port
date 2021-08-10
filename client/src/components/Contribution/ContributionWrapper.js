be11f48abcc8e264b2ff05e2bfb3da059e3c99a8
jong-ah <jonga6431@gmail.com>
2021-08-10 20:22:28 +0900

    Add modal contribution

--- client/src/components/Contribution/ContributionWrapper.js
+++ client/src/components/Contribution/ContributionWrapper.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import store from '../../store/store';
 import Auth from '../../hoc/auth';
+import AlertModal from '../Common/AlertModal/AlertModal';
+
+const END_POINT = process.env.REACT_APP_API_URL;
 
 function ContributionWrapper() {
   const [keyword, setKeyword] = useState('');
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
+  const [userName, setUserName] = useState('');
+
+  let selectOptions = [
+    '게임',
+    '모바일',
+    '보안',
+    '블록체인',
+    '빅데이터',
+    '코딩',
+    '클라우드',
+    '퍼스널 컴퓨팅',
+    'AI/로봇',
+  ];
 
   useEffect(() => {
     Auth(true);
-  });
 
-  let user_name = store.getState().user.signinSuccess[1];
+    setUserName(store.getState().user.signinSuccess[1]);
+  });
 
   function onKeywordHandler(e) {
     setKeyword(e.currentTarget.value);
@@ -28,6 +46,10 @@ function ContributionWrapper() {
   function onSubmitHandler(e) {
     e.preventDefault();
 
+    if (title === '' || content === '' || keyword === '') {
+      return setModalOpen(true);
+    }
+
     let body = {
       contribution_title: title,
       contribution_content: content,
@@ -37,25 +59,32 @@ function ContributionWrapper() {
     return axios
       .post(`${END_POINT}/contribution`, body, { withCredentials: true })
       .then(res => {
-        if (res.status === 200) alert('기고신청이 완료되었습니다.');
-        else alert('기고신청이 실패하였습니다.');
+        if (res.status === 200) {
+          alert('기고신청이 완료되었습니다.');
+          // window.history.back();
+        } else alert('기고신청이 실패하였습니다.');
       });
   }
 
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
-      <div>{user_name}님의 얕고 넓은 지식을 기고해보세요.</div>
+      <div>{userName}님의 얕고 넓은 지식을 기고해보세요.</div>
       <div>
         심사 후에 기고되며,
         <br />
         기고 관리는 마이페이지에 할 수 있어요.
       </div>
       <form onSubmit={e => onSubmitHandler(e)} className="signinform">
-        <input
-          type="text"
-          onChange={e => onKeywordHandler(e)}
-          placeholder="키워드"
-        />
+        <select onChange={e => onKeywordHandler(e)}>
+          <option value="">키워드 선택</option>
+          {selectOptions.map(option => (
+            <option value={option}>{option}</option>
+          ))}
+        </select>
 
         <br />
         <input
@@ -68,7 +97,7 @@ function ContributionWrapper() {
         <textarea
           cols="50"
           rows="10"
-          nChange={e => onContentHandler(e)}
+          onChange={e => onContentHandler(e)}
           placeholder="내용"
           className="contextarea"
         ></textarea>
@@ -76,6 +105,14 @@ function ContributionWrapper() {
         <br />
         <button type="submit">기고 신청</button>
       </form>
+      <div className="alermodalbox">
+        <AlertModal
+          open={modalOpen}
+          close={closeModal}
+          alertString={'모두 입력해야 합니다.'}
+          alertBtn="확인"
+        />
+      </div>
     </>
   );
 }
