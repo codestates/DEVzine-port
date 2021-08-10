4cf1b755f000777c602b68ea466fb7d4b6068ccf
hyoogu <idhyo0o@gmail.com>
2021-08-10 14:28:23 +0900

    Add passport middleware to signin

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,10 +1,11 @@
 const { signUp, signIn, signOut, deleteUser } = require('../controller/user');
+const passport = require('passport');
 const express = require('express');
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signout', signOut);
-router.post('/signin', signIn);
+router.post('/signin', passport.authenticate('local'), signIn);
 router.delete('/delete', deleteUser);
 
 module.exports = router;
