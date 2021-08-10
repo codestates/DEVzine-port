a1cd5f6b102c541d4d30ef5825b3bef204ac1ee3
Haeun Kang <tejavascript@gmail.com>
2021-08-10 10:56:14 +0900

    Fix emails model to newsletters

---
+++ server/Models/Newsletters.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const newsletterSchema = mongoose.Schema({
+    "newsletter_date": {
+        "type": Date,
+        "default": Date.now()
+    },
+	"newsletter_content": String,
+	"newsletter_total_recipients": Number
+});
+
+const Newsletter = mongoose.model("newsletters", newsletterSchema);
+
+module.exports = { Newsletter };
--- server/seeds/insertSeedData.js
+++ server/seeds/insertSeedData.js
@@ -6,8 +6,8 @@ const { Subscriber } = require('../Models/Subscribers');
 const { seedSubscribers } = require('./seed-subscribers');
 const { User } = require('../Models/Users');
 const { seedUsers } = require('./seed-users');
-const { Email } = require('../Models/Emails');
-const { seedEmails } = require('./seed-emails.js');
+const { Newsletter } = require('../Models/Newsletters');
+const { seedNewsletters } = require('./seed-newsletters.js');
 const { VerifiedEmail } = require('../Models/Verifiedemails');
 const { seedVerifiedEmails } = require('./seed-verifiedemails');
 const { Admin } = require('../Models/Admins');
@@ -16,13 +16,13 @@ const { seedAdmins } = require('./seed-admins');
 module.exports = {
 insertSeedData: async (req, res) => {
     try {
-        await Article.create(seedArticles);
-        await Contribution.create(seedContributions);
-        await Subscriber.create(seedSubscribers);
-        await User.create(seedUsers);
-        await Email.create(seedEmails);
-        await VerifiedEmail.create(seedVerifiedEmails);
-        await Admin.create(seedAdmins);
+        // await Article.create(seedArticles);
+        // await Contribution.create(seedContributions);
+        // await Subscriber.create(seedSubscribers);
+        // await User.create(seedUsers);
+        // await Newsletter.create(seedNewsletters);
+        // await VerifiedEmail.create(seedVerifiedEmails);
+        // await Admin.create(seedAdmins);
         res.json({ message: 'Seed success' });
     } catch (err) {
         res.json(err);
