1d6aaed8718ff90894f3ba3f97af59a969afafbf
jong-ah <jonga6431@gmail.com>
2021-08-10 12:09:58 +0900

    Add mypage action

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,9 @@
-import { SIGNIN_USER, SIGNUP_USER, SIGNOUT_USER } from '../_actions/types';
+import {
+  SIGNIN_USER,
+  SIGNUP_USER,
+  SIGNOUT_USER,
+  MYPAGE_USER,
+} from '../_actions/types';
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -10,6 +15,9 @@ export default function (state = {}, action) {
       break;
     case SIGNOUT_USER:
       return (state = {});
+      break;
+    case MYPAGE_USER:
+      return { ...state, signinSuccess: action.payload };
       break;
     default:
       return state;
--- client/src/components/MyPage/MyPageWrapper.js
+++ client/src/components/MyPage/MyPageWrapper.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { mypageUser } from '../../_actions/user_actions';
+
+const END_POINT = process.env.REACT_APP_API_URL;
 
 function MyPageWrapper() {
+  const dispatch = useDispatch();
+
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get(`${END_POINT}/mypage`, {
+          withCredentials: true,
+        })
+        .then(res => {
+          // setUserName(res.data.data.user_name);
+          setUserName('test');
+        })
+        .catch(err => {
+          alert('회원 정보를 받아오는데 실패하였습니다.');
+        });
+    };
+    getData();
+  }, []);
+
+  function onUserNameHandler(e) {
+    setUserName(e.currentTarget.value);
+  }
+
+  function onSubmitHandler(e) {
+    e.preventDefault();
+
+    let body = {
+      user_name: userName,
+    };
+
+    console.log('MyPageWrapper :', body);
+
+    dispatch(mypageUser(body)).then(res => {
+      if (res.payload[2] === 'Patch Success') {
+        alert('정보수정하였습니다.');
+        window.location.href = '/mypage';
+      } else {
+        alert('정보수정을 실패하였습니다.');
+      }
+    });
+  }
+
   return (
     <>
-      MyPageWrapper
+      <form onSubmit={e => onSubmitHandler(e)} className="signinform">
+        <input
+          type="text"
+          onChange={e => onUserNameHandler(e)}
+          placeholder="닉네임"
+          defaultValue={userName}
+        />
+
+        <br />
+        <button type="submit">정보수정</button>
+      </form>
       <br />
       <button onClick={() => (window.location.href = '/contributionupdate')}>
         기고 수정하기
