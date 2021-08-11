cb78d0aa09190af45acee8091634dfd3a28a7300
jong-ah <jonga6431@gmail.com>
2021-08-12 02:17:21 +0900

    Add landing responsive layout

--- client/src/components/Landing/LandingWrapper.js
+++ client/src/components/Landing/LandingWrapper.js
@@ -7,33 +7,53 @@ import landing03 from '../../assets/images/landing03.svg';
 import landing04 from '../../assets/images/landing04.svg';
 
 function LandingWrapper() {
-  const [subscribers, setSubscribers] = useState('1000');
+  const [subscribers, setSubscribers] = useState('200000');
   const [count, setCount] = useState('0');
+  const [ScrollY, setScrollY] = useState(0);
+  const [ScrollActive, setScrollActive] = useState(false);
 
-  useEffect(async () => {
-    const requestGet = await customAxios
-      .get('/landing')
-      .then(res => res.data.data.setSubscribers)
-      .catch(err => alert('회원 수 정보를 받아오는데 실패하였습니다.'));
-
-    setSubscribers('1000');
-    // setSubscribers(requestGet);
-  }, []);
+  // useEffect(async () => {
+  //   const requestGet = await customAxios
+  //     .get('/landing')
+  //     .then(res => res.data.data.setSubscribers);
+  //   .catch(err => alert('회원 수 정보를 받아오는데 실패하였습니다.'));
+  //   setSubscribers(requestGet);
+  // }, []);
 
   useEffect(() => {
     let start = 0;
-    const end = parseInt(subscribers.substring(3));
+    const end = parseInt(subscribers.substring(0, 3));
     if (start === end) return;
 
-    let totalMilSecDur = parseInt(3000);
-    let incrementTime = (totalMilSecDur / end) * 1000;
+    let totalMilSecDur = parseInt(1);
+    let incrementTime = (totalMilSecDur / end) * 3500;
 
     let timer = setInterval(() => {
       start += 1;
-      setCount(String(start) + subscribers.substring(3));
+      setCount(String(start) + ',' + subscribers.substring(3));
       if (start === end) clearInterval(timer);
     }, incrementTime);
-  }, [subscribers]);
+  }, [ScrollActive]);
+
+  useEffect(() => {
+    function scrollListener() {
+      window.addEventListener('scroll', handleScroll);
+    } //  window 에서 스크롤을 감시 시작
+    scrollListener(); // window 에서 스크롤을 감시
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }; //  window 에서 스크롤을 감시를 종료
+  });
+
+  function handleScroll() {
+    if (ScrollY > window.innerHeight) {
+      setScrollY(window.pageYOffset);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setScrollActive(false);
+    }
+  }
 
   return (
     <>
@@ -80,7 +100,7 @@ function LandingWrapper() {
                 <h2>
                   이메일을 통해
                   <br />
-                  매일 받아본다
+                  주기적으로 받아본다
                 </h2>
                 <p>구독신청하면 신청한 이메일로 받아볼 수 있습니다.</p>
               </div>
@@ -106,8 +126,7 @@ function LandingWrapper() {
       <div className="withuscontainer">
         <h2>{count}명</h2>
         구독하고 있어요.
-        <br />
-        많은 분들이 찾는 DEVzine과 함께 해요!
+        <p>많은 분들이 찾는 DEVzine과 함께 해요!</p>
         <div className="landingbottombtn">
           <Button
             subject="구독하기"
