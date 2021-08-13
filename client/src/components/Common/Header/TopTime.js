ef2eddf0b51de67a4e9e6856ac2d7db78862b048
jong-ah <jonga6431@gmail.com>
2021-08-13 14:26:16 +0900

    Add header time secs

--- client/src/components/Common/Header/TopTime.js
+++ client/src/components/Common/Header/TopTime.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 function TopTime() {
-  const [time, setTime] = useState('00:00');
+  const [time, setTime] = useState('00:00:00');
 
   useEffect(() => {
-    setInterval(getTime, 100);
+    setInterval(getTime, 1000);
     return () => {
-      setInterval(getTime, 100);
+      setInterval(getTime, 1000);
     };
   }, []);
 
@@ -17,13 +17,17 @@ function TopTime() {
     const diff = fixDate - currDate;
     let hours = Math.floor(diff / (60 * 60 * 1000));
     let mins = Math.floor((diff - hours * 60 * 60 * 1000) / (60 * 1000));
+    let secs = Math.floor(
+      (diff - hours * 60 * 60 * 1000 - mins * 60 * 1000) / 1000,
+    );
 
     if (hours < 0) hours += 24;
 
     if (hours < 10) hours = `0${hours}`;
     if (mins < 10) mins = `0${mins}`;
+    if (secs < 10) secs = `0${secs}`;
 
-    setTime(`${hours}:${mins}`);
+    setTime(`${hours}:${mins}:${secs}`);
   }
   return <span>{time}</span>;
 }
