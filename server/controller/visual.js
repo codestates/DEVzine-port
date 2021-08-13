5693de5b9029b095b816cc053d2f8cb873bf82ec
Haeun Kang <tejavascript@gmail.com>
2021-08-13 14:34:55 +0900

    Refactor visual controller

--- server/controller/visual.js
+++ server/controller/visual.js
@@ -1,6 +1,6 @@
 const redisClient = require('../config/redis')
-const { User } = require('../Models/Users')
-const { Article } = require('../Models/Articles')
+const { getUserStats } = require('./statfunction/userStats')
+const { getArticleStats } = require('./statfunction/articleStats')
 const { Stat } = require('../Models/Stats')
 
 module.exports = {
@@ -8,133 +8,10 @@ module.exports = {
 	getStatisticsForVisual: async (req, res) => {
 
                 try {
-                        let users = {};
 
-                        users.users_gender = {
-                                users_male: await User.find({
-                                        user_gender: "남자"
-                                }).countDocuments(),
-                                users_female: await User.find({
-                                        user_gender: "여자"
-                                }).countDocuments()
-                        }
-
-                        users.user_generation = {
-                                users_under20: await User.find({
-                                        user_age: "10대"
-                                }).countDocuments(),
-                                users_20: await User.find({
-                                        user_age: "20대"
-                                }).countDocuments(),
-                                users_30: await User.find({
-                                        user_age: "30대"
-                                }).countDocuments(),
-                                users_40: await User.find({
-                                        user_age: "40대"
-                                }).countDocuments(),
-                                users_over50: await User.find({
-                                        user_age: {
-                                                $in: ["50대", "60대 이상"]
-                                        }
-                                }).countDocuments()
-                        }; 
-
-                        users.users_top_position = {};
-                        let positionCount = await User.aggregate([
-                                {
-                                        $group: {
-                                                _id: '$user_position',
-                                                count: {
-                                                        $sum: 1
-                                                }
-                                        }
-                                }
-                        ]).sort({
-                                count: -1
-                        }).limit(5);
-                        positionCount.forEach(position => {
-                                users.users_top_position[position._id] = position.count;
-                        });
-
-                        users.users_top_language = {};
-                        let languageCount = await User.aggregate([
-                                {
-                                        $unwind: "$user_language"
-                                },
-                                {
-                                        $group: {
-                                                _id: '$user_language',
-                                                count: {
-                                                        $sum: 1
-                                                }
-                                        }
-                                }
-                        ]).sort({
-                                count: -1
-                        }).limit(5);
-                        languageCount.forEach(language => {
-                                users.users_top_language[language._id] = language.count;
-                        });
-
-                        let articles = {};
+                        let users = await getUserStats();
+                        let articles = await getArticleStats();
                         
-                        articles.articles_top_hit = await Article.find({},{
-                                article_id: 1,
-                                article_title: 1,
-                                hit: 1
-                        }).sort({
-                                hit: -1, 
-                                article_id: -1
-                        }).limit(5);
-
-                        articles.articles_per_keyword_day = {};
-                        let compareDate = new Date();
-                        compareDate.setHours(compareDate.getHours() + 9);
-                        compareDate.setDate(compareDate.getDate() - 1);
-                        let keywordCountForDay = await Article.aggregate([
-                                {"$match": {
-                                        "article_date": {
-                                                $gt: compareDate
-                                        }
-                                }},
-                                {
-                                        $group: {
-                                                _id: '$article_keyword',
-                                                count: {
-                                                        $sum: 1
-                                                }
-                                        }
-                                }
-                        ]).sort({
-                                count: -1
-                        }).limit(5);
-                        keywordCountForDay.forEach(keyword => {
-                                articles.articles_per_keyword_day[keyword._id] = keyword.count;
-                        });
-
-                        articles.articles_per_keyword_month = {};
-                        compareDate.setDate(compareDate.getDate() - 29);
-                        let keywordCountForMonth = await Article.aggregate([
-                                {"$match": {
-                                        "article_date": {
-                                                $gt: compareDate
-                                        }
-                                }},
-                                {
-                                        $group: {
-                                                _id: '$article_keyword',
-                                                count: {
-                                                        $sum: 1
-                                                }
-                                        }
-                                }
-                        ]).sort({
-                                count: -1
-                        }).limit(5);
-                        keywordCountForMonth.forEach(keyword => {
-                                articles.articles_per_keyword_month[keyword._id] = keyword.count;
-                        });
-
                         const data = { users, articles };
 
                         let curDate = new Date();
