f885867ae984a701454e1aabe76e8848e00fc21f
Haeun Kang <tejavascript@gmail.com>
2021-08-13 14:28:05 +0900

    Add articles stats for visual page

--- server/controller/visual.js
+++ server/controller/visual.js
@@ -1,5 +1,7 @@
 const redisClient = require('../config/redis')
 const { User } = require('../Models/Users')
+const { Article } = require('../Models/Articles')
+const { Stat } = require('../Models/Stats')
 
 module.exports = {
 
@@ -9,16 +11,32 @@ module.exports = {
                         let users = {};
 
                         users.users_gender = {
-                                users_male: await User.find({user_gender: "남자"}).countDocuments(),
-                                users_female: await User.find({user_gender: "여자"}).countDocuments()
+                                users_male: await User.find({
+                                        user_gender: "남자"
+                                }).countDocuments(),
+                                users_female: await User.find({
+                                        user_gender: "여자"
+                                }).countDocuments()
                         }
 
                         users.user_generation = {
-                                users_under20: await User.find({user_age: "10대"}).countDocuments(),
-                                users_20: await User.find({user_age: "20대"}).countDocuments(),
-                                users_30: await User.find({user_age: "30대"}).countDocuments(),
-                                users_40: await User.find({user_age: "40대"}).countDocuments(),
-                                users_over50: await User.find({user_age: {$in: ["50대", "60대 이상"]}}).countDocuments()
+                                users_under20: await User.find({
+                                        user_age: "10대"
+                                }).countDocuments(),
+                                users_20: await User.find({
+                                        user_age: "20대"
+                                }).countDocuments(),
+                                users_30: await User.find({
+                                        user_age: "30대"
+                                }).countDocuments(),
+                                users_40: await User.find({
+                                        user_age: "40대"
+                                }).countDocuments(),
+                                users_over50: await User.find({
+                                        user_age: {
+                                                $in: ["50대", "60대 이상"]
+                                        }
+                                }).countDocuments()
                         }; 
 
                         users.users_top_position = {};
@@ -26,10 +44,14 @@ module.exports = {
                                 {
                                         $group: {
                                                 _id: '$user_position',
-                                                count: {$sum: 1}
+                                                count: {
+                                                        $sum: 1
+                                                }
                                         }
                                 }
-                        ]).sort({count: -1}).limit(5);
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
                         positionCount.forEach(position => {
                                 users.users_top_position[position._id] = position.count;
                         });
@@ -42,44 +64,95 @@ module.exports = {
                                 {
                                         $group: {
                                                 _id: '$user_language',
-                                                count: {$sum: 1}
+                                                count: {
+                                                        $sum: 1
+                                                }
                                         }
                                 }
-                        ]).sort({count: -1}).limit(5);
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
                         languageCount.forEach(language => {
                                 users.users_top_language[language._id] = language.count;
                         });
 
                         let articles = {};
+                        
+                        articles.articles_top_hit = await Article.find({},{
+                                article_id: 1,
+                                article_title: 1,
+                                hit: 1
+                        }).sort({
+                                hit: -1, 
+                                article_id: -1
+                        }).limit(5);
+
+                        articles.articles_per_keyword_day = {};
+                        let compareDate = new Date();
+                        compareDate.setHours(compareDate.getHours() + 9);
+                        compareDate.setDate(compareDate.getDate() - 1);
+                        let keywordCountForDay = await Article.aggregate([
+                                {"$match": {
+                                        "article_date": {
+                                                $gt: compareDate
+                                        }
+                                }},
+                                {
+                                        $group: {
+                                                _id: '$article_keyword',
+                                                count: {
+                                                        $sum: 1
+                                                }
+                                        }
+                                }
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
+                        keywordCountForDay.forEach(keyword => {
+                                articles.articles_per_keyword_day[keyword._id] = keyword.count;
+                        });
+
+                        articles.articles_per_keyword_month = {};
+                        compareDate.setDate(compareDate.getDate() - 29);
+                        let keywordCountForMonth = await Article.aggregate([
+                                {"$match": {
+                                        "article_date": {
+                                                $gt: compareDate
+                                        }
+                                }},
+                                {
+                                        $group: {
+                                                _id: '$article_keyword',
+                                                count: {
+                                                        $sum: 1
+                                                }
+                                        }
+                                }
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
+                        keywordCountForMonth.forEach(keyword => {
+                                articles.articles_per_keyword_month[keyword._id] = keyword.count;
+                        });
 
                         const data = { users, articles };
+
+                        let curDate = new Date();
+                        curDate.setHours(curDate.getHours() + 9);
+
+                        await Stat.create({
+                                stat_datetime: curDate,
+                                stat_content: data
+                        });
+
                         redisClient.setex('visualsActivated', 10, JSON.stringify(data));
                         return res.status(200).json({data, "message": "success", "source": "DB"});
-                        // if (!maleCount) { // 어딘가에 사용될 404
-                        //         return res.status(404).json({"message": "Not found"});
-                        // }
+                        
                 } catch (err) {
                         console.log(err);
                         return res.status(500).send(err);
                 }
-        // TODO: 08.12 Users 작업 완료 -> Articles 통계 작업 필요 (FE 측에서 visuals 부분 확인 후 시작)
-        // {
-        //     "data" : {
-        //         "articles_top_hit" : [
-        //             { "article_id": number,
-        //                 "article_title": string,
-        //                 "hit": number
-        //             },
-        //             ...
-        //         ],
-        //         "article_per_keyword_month" : {
-        //             "keyword": number,
-        //             "keyword2" : number,
-        //         },
-        //         "article_per_keyword_day" : {
-        //             "keyword": number,
-        //             "keyword2" : number,
-        //         },
-        // + 완료된 통계 DB 에 저장 
+
 	}
+
 };
