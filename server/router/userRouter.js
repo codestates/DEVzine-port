888652f6ebccf0079d5a19b7702061b1afb68962
hyoogu <idhyo0o@gmail.com>
2021-08-16 13:24:45 +0900

    Make token auth

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signout', signOut);
-router.post('/signin', passport.authenticate('local'), signIn);
+router.post('/signin', signIn);
 router.delete('/delete', deleteUser);
 
 module.exports = router;
