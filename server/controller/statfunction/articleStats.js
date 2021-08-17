4a763763c0b73144b9c4fa7ed874e69f40ade436
Haeun Kang <tejavascript@gmail.com>
2021-08-17 21:51:27 +0900

    Add missing status code for visuals

--- server/controller/statfunction/articleStats.js
+++ server/controller/statfunction/articleStats.js
@@ -4,76 +4,85 @@ module.exports = {
 
         getArticleStats: async () => {
 
-                let articles = {};
-                                
-                articles.articles_top_hit = await Article.find({},{
-                        article_id: 1,
-                        article_title: 1,
-                        hit: 1
-                }).sort({
-                        hit: -1, 
-                        article_id: -1
-                }).limit(5);
+                try {
 
-                articles.articles_per_keyword_day = {};
+                        let articles = {};
+                                        
+                        articles.articles_top_hit = await Article.find({},{
+                                article_id: 1,
+                                article_title: 1,
+                                hit: 1
+                        }).sort({
+                                hit: -1, 
+                                article_id: -1
+                        }).limit(5);
 
-                let compareDate = new Date(Date.now());
-                compareDate.setHours(compareDate.getHours() + 9);
-                compareDate.setDate(compareDate.getDate() - 1);
+                        articles.articles_per_keyword_day = {};
 
-                let keywordCountForDay = await Article.aggregate([
-                        {
-                                $match: {
-                                        article_date: {
-                                                $gt: compareDate
+                        let compareDate = new Date(Date.now());
+                        compareDate.setHours(compareDate.getHours() + 9);
+                        compareDate.setDate(compareDate.getDate() - 1);
+
+                        let keywordCountForDay = await Article.aggregate([
+                                {
+                                        $match: {
+                                                article_date: {
+                                                        $gt: compareDate
+                                                }
+                                        }
+                                },
+                                {
+                                        $group: {
+                                                _id: '$article_keyword',
+                                                count: {
+                                                        $sum: 1
+                                                }
                                         }
                                 }
-                        },
-                        {
-                                $group: {
-                                        _id: '$article_keyword',
-                                        count: {
-                                                $sum: 1
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
+
+                        keywordCountForDay.forEach(keyword => {
+                                articles.articles_per_keyword_day[keyword._id] = keyword.count;
+                        });
+
+                        articles.articles_per_keyword_month = {};
+
+                        compareDate.setDate(compareDate.getDate() - 29);
+
+                        let keywordCountForMonth = await Article.aggregate([
+                                {
+                                        $match: {
+                                                article_date: {
+                                                        $gt: compareDate
+                                                }
+                                        }
+                                },
+                                {
+                                        $group: {
+                                                _id: '$article_keyword',
+                                                count: {
+                                                        $sum: 1
+                                                }
                                         }
                                 }
-                        }
-                ]).sort({
-                        count: -1
-                }).limit(5);
+                        ]).sort({
+                                count: -1
+                        }).limit(5);
+                        
+                        keywordCountForMonth.forEach(keyword => {
+                                articles.articles_per_keyword_month[keyword._id] = keyword.count;
+                        });
 
-                keywordCountForDay.forEach(keyword => {
-                        articles.articles_per_keyword_day[keyword._id] = keyword.count;
-                });
+                        return articles;
+                        
+                } catch (err) {
 
-                articles.articles_per_keyword_month = {};
-
-                compareDate.setDate(compareDate.getDate() - 29);
-
-                let keywordCountForMonth = await Article.aggregate([
-                        {
-                                $match: {
-                                        article_date: {
-                                                $gt: compareDate
-                                        }
-                                }
-                        },
-                        {
-                                $group: {
-                                        _id: '$article_keyword',
-                                        count: {
-                                                $sum: 1
-                                        }
-                                }
-                        }
-                ]).sort({
-                        count: -1
-                }).limit(5);
-                
-                keywordCountForMonth.forEach(keyword => {
-                        articles.articles_per_keyword_month[keyword._id] = keyword.count;
-                });
-
-                return articles;
+                        console.log(err);
+                        return false;
+                        
+                }
 
         }
 
