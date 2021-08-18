d0209087c65050af1d4675909f0bc7ebe43bac3e
bmaner <bmanerdaniel@gmail.com>
2021-08-18 18:10:22 +0900

    Add charts of visual page

--- client/src/components/Visual/VisualWrapper.js
+++ client/src/components/Visual/VisualWrapper.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { customAxios } from '../../utils/customAxios';
+import { UserTopLanguage, UserTopPosition } from './RoundCharts';
+import { ArticlesTopHit } from './BarChart';
 
 function VisualWrapper() {
-  return (
+  const [ChartData, setChartData] = useState();
+
+  useEffect(async () => {
+    await customAxios
+      .get('/visual')
+      .then(res => setChartData(res.data.data))
+      .catch(err => alert(err));
+  }, []);
+
+  return ChartData ? (
     <>
-      <div className="visualwrapper">
+      <div className="visualcontainer">
         <div className="container">
           <div className="row">
             <div className="co-sm-4">
-              <div className="visualtest">VisualWrapper</div>
+              <div className="visualwrapper">
+                <section className="chartcontainer"></section>
+                <section className="chartcontainer"></section>
+                <section className="chartcontainer">
+                  <ArticlesTopHit data={ChartData} />
+                </section>
+                <section className="chartcontainer"></section>
+                <section className="chartcontainer">
+                  <UserTopPosition data={ChartData} />
+                </section>
+                <section className="chartcontainer">
+                  <UserTopLanguage data={ChartData} />
+                </section>
+                <div className="visualwrapper-footer" />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  );
+  ) : null;
 }
 
 export default VisualWrapper;
