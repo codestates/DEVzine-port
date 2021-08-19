76483ed08723c3788aaba001c4e7acb3aa7c5300
Haeun Kang <tejavascript@gmail.com>
2021-08-19 14:41:37 +0900

    Add delete request for contribution

--- server/controller/contribution.js
+++ server/controller/contribution.js
@@ -33,18 +33,20 @@ module.exports = {
                         let contribution_date = new Date(Date.now());
                         contribution_date.setHours(contribution_date.getHours() + 9);
 
-                        let newContribution = await Contribution.create({
-                                user_email,
-                                contribution_title,
-                                contribution_content,
-                                contribution_keyword,
-                                contribution_date,
-                                contribution_url: `temp${Math.random()}`
-                        });
+                        let newContribution = await Contribution.create(
+                                {
+                                        user_email,
+                                        contribution_title,
+                                        contribution_content,
+                                        contribution_keyword,
+                                        contribution_date,
+                                        contribution_url: `temp${Math.random()}`
+                                }
+                        );
 
                         const contribution_url = `${process.env.DEVZINE_CLIENT_ENDPOINT}/article/con-${newContribution.contribution_id}`;
 
-                        let temp = await Contribution.updateOne(
+                        await Contribution.updateOne(
                                 {
                                         contribution_id: newContribution.contribution_id
                                 }, {
@@ -69,26 +71,76 @@ module.exports = {
 	},
 
         deleteContribution: async (req, res) => {
-        
-        // TODO: 사용자가 원하는 글을 매거진에서 삭제 요청한다. 
-        // req.body
-        // { 
-        //         "contribution_id": number
-        // }
-        // status: 200
-        // {
-        //     "message" : "Delete request success"
-        // }
-        // status:404
-        // {
-        //     "message": "Not found"
-        // }
 
-        return res.send('delete contribution');
+                try {
+
+
+                        const { _id, user_email } = req.user;
+                        const contribution_id = Number(req.params.contributionid);
+
+                        let user = await User.findOne(
+                                { 
+                                        _id
+                                }
+                        );
+                        if (!user) {
+                                return res.status(404).json(
+                                        {
+                                                "message": "User not found"
+                                        }
+                                );
+                        }
+
+                        const contributionForDeletion = await Contribution.findOne(
+                                {
+                                        contribution_id
+                                }
+                        )
+
+                        if (!contributionForDeletion) {
+                                return res.status(404).json(
+                                        {
+                                                "message": "Contribution not found"
+                                        }
+                                );
+                        }
+
+                        if (contributionForDeletion.user_email !== user_email) {
+                                return res.status(403).json(
+                                        {
+                                                "message": "Unauthorized user"
+                                        }
+                                );
+                        }
+
+                        await Contribution.updateOne(
+                                {
+                                        contribution_id
+                                }, {
+                                        $set: {
+                                                status: 102
+                                        }
+                                }
+                        )
+
+                        return res.status(200).json(
+                                {
+                                        "message": "Delete request success"
+                                }
+                        );
+
+                } catch (err) {
+
+                        console.log(err)
+                        return res.status(500).send(err);
+                
+                }
         
         },
 
         updateContribution: async (req, res) => {
+
+                // 날짜 바뀜 
         
         // TODO: 사용자가 원하는 글에 대해 수정 요청한다.
         // req.body
