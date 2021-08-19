55a613f0f93feb5545a8063097fd1d8db88ffaab
Haeun Kang <tejavascript@gmail.com>
2021-08-19 13:17:38 +0900

    Add passport middleware for contribution router

--- server/router/contributionRouter.js
+++ server/router/contributionRouter.js
@@ -1,9 +1,10 @@
 const { addContribution, deleteContribution, updateContribution } = require('../controller/contribution');
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
-router.post('/', addContribution);
-router.delete('/', deleteContribution);
-router.patch('/', updateContribution);
+router.post('/', passport.authenticate('jwt', { session: false }), addContribution);
+router.delete('/', passport.authenticate('jwt', { session: false }), deleteContribution);
+router.patch('/', passport.authenticate('jwt', { session: false }), updateContribution);
 
 module.exports = router;
