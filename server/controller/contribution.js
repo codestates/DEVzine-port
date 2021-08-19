7971a12192ee80ce92409f5d33240b7e88bff49e
Haeun Kang <tejavascript@gmail.com>
2021-08-19 14:03:38 +0900

    Add contribution post request

--- server/controller/contribution.js
+++ server/controller/contribution.js
@@ -1,25 +1,70 @@
+const { User } = require('../Models/Users');
+const { Contribution } = require('../Models/Contributions');
+require('dotenv').config();
+
 module.exports = {
 
 	addContribution: async (req, res) => {
-        
-        // TODO: 사용자가 원하는 글을 매거진에 기고한다.
-        // req.body
-        // { 
-        //     "contribution_title" : "string",
-        //     "contribution_url" : "string",
-        //     "contribution_content" : "string",
-        //     "contibution_keyword" : "string",
-        // }
-        // status: 200
-        // {
-        //     "message" : "Contribution request success"
-        // }
-        // status:404
-        // {
-        //     "message": "Not found"
-        // }
+                
+                try {
+                        const { 
+                                _id, 
+                                user_email 
+                        } = req.user;
+                        const { 
+                                contribution_title, 
+                                contribution_content,
+                                contribution_keyword
+                        } = req.body;
 
-        return res.send('contribution');
+                        let user = await User.findOne(
+                                { 
+                                        _id
+                                }
+                        );
+                        if (!user) {
+                                return res.status(404).json(
+                                        {
+                                                "message": "Not found"
+                                        }
+                                );
+                        }
+
+                        let contribution_date = new Date(Date.now());
+                        contribution_date.setHours(contribution_date.getHours() + 9);
+
+                        let newContribution = await Contribution.create({
+                                user_email,
+                                contribution_title,
+                                contribution_content,
+                                contribution_keyword,
+                                contribution_date,
+                                contribution_url: `temp${Math.random()}`
+                        });
+
+                        const contribution_url = `${process.env.DEVZINE_CLIENT_ENDPOINT}/article/con-${newContribution.contribution_id}`;
+
+                        let temp = await Contribution.updateOne(
+                                {
+                                        contribution_id: newContribution.contribution_id
+                                }, {
+                                        $set: {
+                                                contribution_url
+                                        }
+                                })
+                
+                        return res.status(200).json(
+                                {
+                                        "message": "Contribution request success"
+                                }
+                        );
+
+                } catch (err) {
+
+                        console.log(err)
+                        return res.status(500).send(err);
+                
+                }
 
 	},
 
