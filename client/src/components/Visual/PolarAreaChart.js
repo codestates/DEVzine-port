0aac10dc4fa9ff02c4224103798e59e7df3cf621
bmaner <bmanerdaniel@gmail.com>
2021-08-20 17:51:56 +0900

    Fix chart mount/unmount function and Add react-spring animation

--- client/src/components/Visual/PolarAreaChart.js
+++ client/src/components/Visual/PolarAreaChart.js
@@ -1,18 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useTransition, animated } from 'react-spring';
 import Chart from 'react-apexcharts';
+import { userGenerationDataConvert } from '../../utils/userGenerationDataConvert';
 
-export function UserGender({ data }) {
+export function UserAge({ data }) {
+  const [ScrollPosition, setScrollPosition] = useState(0);
+  const [Indicator, setIndicator] = useState(false);
+  const transition = useTransition(Indicator, {
+    from: { x: -100, opacity: 0, skew: '20deg' },
+    enter: { x: 0, opacity: 1, skew: '0deg' },
+    leave: { x: 100, opacity: 0, skew: '20deg' },
+  });
+  let rect = 500;
+
+  const onScroll = () => {
+    setScrollPosition(window.pageYOffset);
+    if (ScrollPosition > 1200 && ScrollPosition < 1200 + rect) {
+      setIndicator(true);
+    } else {
+      setIndicator(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    // 컴포넌트가 언마운트 되기 직전에 이벤트를 끝낸다. 메모리 누수 방지
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [ScrollPosition]);
+
   let options = {
-    colors: ['#ffc803', '#ffe33e'],
+    colors: ['#d9d9d9', '#f2f2f2', '#ffe33e', '#ffdd14', '#ffc803'],
     fill: {
       opacity: 0.9,
-      colors: ['#ffc803', '#ffe33e'],
+      colors: ['#d9d9d9', '#f2f2f2', '#ffe33e', '#ffdd14', '#ffc803'],
     },
     legend: {
       labels: {
-        colors: ['#ffc803', '#ffe33e'],
+        colors: ['#d9d9d9', '#f2f2f2', '#ffe33e', '#ffdd14', '#ffc803'],
       },
       markers: {
-        fillColors: ['#ffc803', '#ffe33e'],
+        fillColors: ['#d9d9d9', '#f2f2f2', '#ffe33e', '#ffdd14', '#ffc803'],
       },
     },
     chart: {
@@ -38,20 +64,24 @@ export function UserGender({ data }) {
   let series = [];
   let labels = [];
 
-  for (const [key, value] of Object.entries(data.users.users_gender)) {
+  for (const [key, value] of Object.entries(data.users.user_generation)) {
     series.push(value);
-    labels.push(key === 'users_male' ? '남자' : '여자');
+    labels.push(userGenerationDataConvert(key));
   }
 
   options.labels = labels;
 
-  return (
-    <Chart
-      options={options}
-      series={series}
-      type="polarArea"
-      width={500}
-      height={500}
-    />
+  return transition((style, item) =>
+    item ? (
+      <animated.div style={style}>
+        <Chart
+          options={options}
+          series={series}
+          type="polarArea"
+          width={500}
+          height={500}
+        />
+      </animated.div>
+    ) : null,
   );
 }
--- client/src/components/Visual/VisualWrapper.js
+++ client/src/components/Visual/VisualWrapper.js
@@ -1,66 +1,20 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { customAxios } from '../../utils/customAxios';
 import { UserTopLanguage, UserTopPosition } from './RoundCharts';
 import { ArticlesTopHit, UserAgeAndGender } from './BarChart';
-import { UserGeneration } from './TreeMapChart';
+import { ArticlesKeywordAccumulated } from './TreeMapChart';
 import { ArticlesKeyword } from './LineCharts';
+import { UserAge } from './PolarAreaChart';
 
 function VisualWrapper() {
   const [ChartData, setChartData] = useState('');
-  const [Pos, setPos] = useState(0);
-  const [Indicator, setIndicator] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
-  const chartcontainer = document.querySelectorAll('.chartcontainer');
-
-  // function onScroll() {
-  //   setPos(window.scrollY);
-  //   // determineIdx()
-  //   determineIdx(Pos);
-  // }
-
-  // const determinePos = useCallback(() => {
-  //   setPos(window.scrollY);
-  // }, []);
-
-  // const determineIdx = Pos => {
-  //   for (let i = 0; i < chartcontainer.length; i++) {
-  //     if (
-  //       Pos > chartcontainer[i].offsetTop - window.outerHeight / 3 &&
-  //       Pos <
-  //         chartcontainer[i].offsetTop -
-  //           window.outerHeight / 2 +
-  //           chartcontainer[i].offsetHeight
-  //     ) {
-  //       setIndicator(
-  //         Indicator.map((el, idx) => {
-  //           if (i === idx) {
-  //             return true;
-  //           } else {
-  //             return false;
-  //           }
-  //         }),
-  //       );
-  //     }
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', onScroll);
-  //   return () => {
-  //     window.addEventListener('scroll', onScroll);
-  //   };
-  // }, [Pos]);
 
   useEffect(async () => {
     await customAxios
       .get('/visual')
-      .then(res => setChartData(res.data.data))
+      .then(res => {
+        setChartData(res.data.data);
+      })
       .catch(err => alert(err));
   }, []);
 
@@ -88,7 +42,12 @@ function VisualWrapper() {
                 </div>
                 <div className="chartcontainer">
                   <section className="chartwrapper">
-                    <UserGeneration data={ChartData} />
+                    <UserAge data={ChartData} />
+                  </section>
+                </div>
+                <div className="chartcontainer">
+                  <section className="chartwrapper">
+                    <ArticlesKeywordAccumulated data={ChartData} />
                   </section>
                 </div>
                 <div className="chartcontainer">
