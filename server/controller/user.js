d9130e45d7ad85a4eda4e7ad680cdd1f626f27f3
hyoogu <idhyo0o@gmail.com>
2021-08-21 15:16:21 +0900

    Refactor code convention

--- server/controller/user.js
+++ server/controller/user.js
@@ -73,12 +73,10 @@ module.exports = {
           // maxAge: 24 * 60 * 60 * 1000, // 있든 말든 상관 없는듯
           // domain: 'devzine-port.com', path: '/' // 이건 넣으면 걍 안됨;;
         });
-        res
-          .status(200)
-          .send({
-            data: { user_name: req.user.user_name },
-            message: 'Login success',
-          });
+        res.status(200).send({
+          data: { user_name: req.user.user_name },
+          message: 'Login success',
+        });
       }
     );
   },
