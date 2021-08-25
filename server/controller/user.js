a6fa5d83c527e0493b8cf06b1238203c772f7af3
hyoogu <idhyo0o@gmail.com>
2021-08-26 00:29:51 +0900

    Fix typo

--- server/controller/user.js
+++ server/controller/user.js
@@ -85,7 +85,7 @@ module.exports = {
           httpOnly: true,
           sameSite: 'None',
           secure: true,
-          domain: ' https://devzine-port.com',
+          domain: 'https://devzine-port.com',
           // maxAge: 24 * 60 * 60 * 1000, // 있든 말든 상관 없는듯
           // domain: 'devzine-port.com', path: '/' // 이건 넣으면 걍 안됨;;
         });
