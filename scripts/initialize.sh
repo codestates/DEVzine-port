4ab3ba114a7a02aad2594c7507e0d22031fb0942
Hyoseung Kwon <idhyo0o@gmail.com>
2022-02-03 11:05:53 +0900

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -6,6 +6,9 @@ npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt install redis-server
+sudo mkdir -p /var/log/redis/ 
+sudo touch /var/log/redis/redis-server.log 
+sudo chown redis:redis /var/log/redis/redis-server.log
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
