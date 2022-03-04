47d185e554748036eba33db71753d31d40f00356
Haeun Kang <75682050+vodkamitlime@users.noreply.github.com>
2022-03-05 02:21:57 +0900

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,14 +5,8 @@ npm install
 npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get upgrade
-wget https://download.redis.io/releases/redis-6.2.6.tar.gz
-tar xzf redis-6.2.6.tar.gz
-cd redis-6.2.6
-make
-apt-install redis-server
-sudo mkdir -p /var/log/redis/ 
-sudo touch /var/log/redis/redis-server.log 
-sudo chown redis:redis /var/log/redis/redis-server.log
+sudo apt install redis-server
+sudo chown -R redis:redis /etc/redis/redis.conf
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
