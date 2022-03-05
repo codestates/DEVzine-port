05e1ff4676939ae42df6157799426b7b7dd5e0e8
Haeun Kang <tejavascript@gmail.com>
2022-03-06 03:11:57 +0900

    fix:conflicts

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/DEVzine-port/server
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt install npm
 npm install
 npm install pm2@latest -g
-sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install authbind
+sudo apt install redis-server
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
